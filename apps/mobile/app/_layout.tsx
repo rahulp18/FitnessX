4089b002fc0894d469501dd4051fcc54494bb8ac
rahulp18 <pradhanrahul3346@gmail.com>
2024-08-27 13:38:27 +0530

    Frontend setup

---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "../global.css";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/font/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="flex items-center justify-center flex-1">
+      <Text className="text-red-500 text-3xl">index</Text>
+    </View>
+  );
+};
+
+export default index;
